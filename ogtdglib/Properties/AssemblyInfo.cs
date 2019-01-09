02300f765a96583a07426edcf086bc01d30f8dbe
timotheyca <timotheyca@users.noreply.github.com>
2019-01-09 22:34:06 +0300

    Update AssemblyInfo.cs

--- ogtdglib/Properties/AssemblyInfo.cs
+++ ogtdglib/Properties/AssemblyInfo.cs
@@ -33,4 +33,3 @@ using System.Runtime.InteropServices;
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.0.*")]
-[assembly: ObfuscateAssembly(true)]
