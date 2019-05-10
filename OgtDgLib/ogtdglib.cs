83d2b1bed817498e03dbbd4d3c2af15632e349dc
timotheyca <timotheyca@users.noreply.github.com>
2019-05-10 22:49:40 +0300

    Update ogtdglib.cs

--- OgtDgLib/ogtdglib.cs
+++ OgtDgLib/ogtdglib.cs
@@ -3,14 +3,26 @@ using JetBrains.Annotations;
 
 namespace OgtDgLib
 {
+    /// <inheritdoc />
+    /// <summary>
+    /// Class for using lib as Mod
+    /// </summary>
     [PublicAPI]
     public class OgtDgLib:Mod
     {
+        /// <inheritdoc />
+        /// <summary>
+        /// Constructor for Debug.Log
+        /// </summary>
         public OgtDgLib()
         {
             Debug.Log("OgtDgLib loading");
         }
 
+        /// <inheritdoc />
+        /// <summary>
+        /// Loads before 
+        /// </summary>
         public override Priority priority => Priority.Highest;
     }
 }
